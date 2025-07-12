7baa216ebf1a67be11f4012b41cc3dedbc6f8242
Fred090821 <119926444+Fred090821@users.noreply.github.com>
2025-07-12 10:09:07 +0100

    Update DuplicateCardNumberException.java

--- src/main/java/com/example/credit_card_api/exception/DuplicateCardNumberException.java
+++ src/main/java/com/example/credit_card_api/exception/DuplicateCardNumberException.java
@@ -3,7 +3,6 @@ package com.example.credit_card_api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateCardNumberException extends RuntimeException {
     public DuplicateCardNumberException(String message) {
         super(message);
